73147a961a3b1131dcf230d9e8a14b10665c4dc7
Stern Huang <stern.huang@gmail.com>
2018-02-23 12:23:19 -0800

    Add string parsing, validation, and comparison methods.

---
+++ verification.js
@@ -0,0 +1,58 @@
+function stringToDigitArray(inputString) {
+  var digits = [];
+
+  var charArray = inputString.split("");
+  for (var i = 0; i < charArray.length; ++i) {
+    digits.push(parseInt(charArray[i]));
+  }
+  return digits;
+}
+
+// input should be an array of digits.
+function isValidArray(userAnswer) {
+  // Check for NaN (created by letters in the given string).
+  for (var i = 0; i < userAnswer.length; ++i) {
+    if (userAnswer[i] == NaN) {
+      return false;
+    }
+  }
+
+  // Check for duplicates.
+  if (hasDuplicates(userAnswer)) {
+    return false;
+  }
+
+  // Check for length.
+  if (userAnswer.length != 4) {
+    return false;
+  }
+
+  return true;
+}
+
+function hasDuplicates(array) {
+  for (var i = 0; i < array.length; ++i) {
+    var value = array[i];
+    if (array.indexOf(value) !== i) {
+      return true;
+    }
+  }
+  return false;
+}
+
+
+// a and b should be arrays of 4 distinct digits of size 4.
+function verify(a, b) {
+  var correctCount = 0;
+  for (var i = 0; i < 4; i++) {
+    if (a[i] == b[i]) {
+      correctCount = correctCount + 1;
+    }
+  }
+  
+  var match = a.filter(function(n) {
+    return b.indexOf(n) !== -1;
+  }).length;
+
+  return [correctCount, match - correctCount];
+}
