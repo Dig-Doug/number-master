690cb3bab93bea2b5ed7b4c387aa76cddb04c9b1
Stern Huang <stern.huang@gmail.com>
2018-02-23 14:11:14 -0800

    Fix typo in numTries to numTriesLeft

--- functions/play_again.js
+++ functions/play_again.js
@@ -4,7 +4,7 @@ const strings = require('./strings');
 const playAgain = app => {
     const data = initData(app);
     data.secretNumber = generatePassword.generatePassword(4);
-    data.numTries = 10;
+    data.numTriesLeft = 10;
     return app.ask(app.buildRichResponse()
             .addSimpleResponse({
                 speech: 'OK, new game. Please guess a four digit number',
