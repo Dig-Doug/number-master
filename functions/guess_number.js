82f6e72be5d911990a9181953c5bf7bf40e823e0
Doug Roeper <douglas.roeper@gmail.com>
2018-02-23 15:07:24 -0800

    Fixed lose

--- functions/guess_number.js
+++ functions/guess_number.js
@@ -39,7 +39,19 @@ const guessNumber = app => {
     const answer = verification.verify(userGuessArray, secretNumber);
 
     data.numTriesLeft = data.numTriesLeft - 1;
-    if (answer[0] === NUM_DIGITS) {
+    if (data.numTriesLeft <= 0) {
+        // Lose
+        const card = app.buildBasicCard(
+            strings.general.lose + " The answer was " + data.secretNumber.join(''))
+            .setImage(strings.general.loseImage, strings.general.loseImageAlt);
+
+        return app.ask(app.buildRichResponse()
+                .addSimpleResponse(playSound(strings.general.loseSound))
+                .addBasicCard(card)
+                .addSuggestions([strings.suggestions.play_again]),
+            strings.general.noInputs);
+    }
+    else if (answer[0] === NUM_DIGITS) {
         // Win
         const card = app.buildBasicCard(playSound(strings.general.winSound, strings.general.win))
             .setImage(strings.general.winImage, strings.general.winImageAlt);
@@ -54,23 +66,7 @@ const guessNumber = app => {
 
         return app.ask(richResponse, strings.general.noInputs);
     }
-    else if (data.numTriesLeft == 0) {
-        // Lose
-        const card = app.buildBasicCard(
-            strings.general.lose + " The answer was " + data.secretNumber.join(''))
-            .setImage(strings.general.loseImage, strings.general.loseImageAlt);
-    
-        return app.ask(app.buildRichResponse()
-                .addSimpleResponse(playSound(strings.general.loseSound))
-                .addBasicCard(card)
-                .addSuggestions([
-                  generateSuggestion(),
-                  generateSuggestion(),
-                  generateSuggestion(),
-                  strings.suggestions.giveup,
-                  strings.suggestions.play_again]),
-            strings.general.noInputs);
-    } else {
+    else {
         const response = `You got ${answer[0]} digit in the correct position, and ${answer[1]} digit in the wrong position. You have ${data.numTriesLeft} tries left. Cheat: ${JSON.stringify(data)}`;
         return app.ask(app.buildRichResponse()
                 .addSimpleResponse({
