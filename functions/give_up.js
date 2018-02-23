55c67e5775566aec705b9368ca9115f9e9d2c0c9
Doug Roeper <douglas.roeper@gmail.com>
2018-02-23 15:50:13 -0800

    Fix

--- functions/give_up.js
+++ functions/give_up.js
@@ -4,6 +4,8 @@ const strings = require('./strings');
 const giveUp = app => {
     const data = initData(app);
     const secretNumber = data.secretNumber;
+    data.secretNumber = generatePassword.generatePassword(4);
+    data.numTriesLeft = 10;
     return app.ask(app.buildRichResponse()
             .addSimpleResponse(`The secret number is ${secretNumber.join('')}`)
             .addSuggestions(
